f0b02974e4b24467e8baa401eaccd6e3d451c731
JeelChandegra <97683052+JeelChandegra@users.noreply.github.com>
2024-09-24 21:23:05 +0530

    Add files via upload

---
+++ Macros.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+#define loop(n) for(int i = 0; i < n;  i++)
+#define w(n) while(n--)
+
+int main()
+{
+    
+   
+
+    return 0;
+}

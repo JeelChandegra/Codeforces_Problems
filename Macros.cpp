468b020411a96becff08cd879296e3d599286491
JeelChandegra <97683052+JeelChandegra@users.noreply.github.com>
2025-04-04 14:11:45 +0530

    Update Macros.cpp

--- Macros.cpp
+++ Macros.cpp
@@ -1,14 +1,53 @@
-#include<bits/stdc++.h>
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-#define loop(n) for(int i = 0; i < n;  i++)
-#define w(n) while(n--)
 
-int main()
-{
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pii>
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define sz(x) (int)(x).size()
+#define endl '\n'
+
+
+
+
+#define read_array(arr, size) std::vector<int> arr(size); for(int i = 0; i < size; i++) cin >> arr[i];
+#define print_array(arr) for(auto x : arr) cout << x << " "; cout << "\n";
+#define read_vector(vec, size) vector<int> vec(size); for(int i = 0; i < size; i++) cin >> vec[i];
+#define print_vector(vec) for(auto x : vec) cout << x << " "; cout << "\n";
+
+
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX;
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+
+void solve() {
+    
+        
+
+}
+
+int32_t main() {
+    fastIO();
     
    
+
+
 
     return 0;
 }
